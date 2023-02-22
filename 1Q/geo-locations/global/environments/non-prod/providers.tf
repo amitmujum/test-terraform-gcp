69a7002f18ca3e213ebe040839f227bae12660e2
amitmujum <amitmujumdar25@gmail.com>
2023-02-22 17:54:09 +0530

    Create providers.tf

---
+++ 1Q/geo-locations/global/environments/non-prod/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=4.30.0"
+    }
+  }
+  backend "gcs" {
+    bucket      = "tf-state-np"
+    prefix      = "env/global/non-prod"
+    credentials = "/path-to-sa-cicd.json"
+  }
+}
+
+provider "google" {
+  credentials = var.credentials
+  region      = var.region
+  zone        = var.zone
+  project     = var.project
+}
+
